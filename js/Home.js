12c77818edf48f82e3e7f8224cda6b31b99e763b
esamy99 <84810033+esamy99@users.noreply.github.com>
2021-05-25 22:09:00 +0200

    Add files via upload

---
+++ js/Home.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $("div").animate({
+      left: '250px',
+      opacity: '0.5',
+      height: '150px',
+      width: '150px'
+    });
+  });
+});
